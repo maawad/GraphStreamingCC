b7a8348dc0544b1ab5ca7697d2afdb99641ec134
Evan West <evtwest@yahoo.com>
2021-06-07 12:33:11 -0400

    added the basics of the speed test

--- test/experiment/BCHMK_Graph.cpp
+++ test/experiment/BCHMK_Graph.cpp
@@ -7,59 +7,6 @@
 #include "../util/graph_verifier.h"
 #include "../util/graph_gen.h"
 
-/*
- * Function which is run in a seperate thread and will query
- * the graph for the number of updates it has processed
- * the thread writes that information to a given file
- * @param total       the total number of edge updates
- * @param g           the graph object to query
- * @param start_time  the time that we started stream ingestion
- */
-void query_insertions(uint64_t total, Graph *g, std::chrono::steady_clock::time_point start_time) {
-  total = total * 2;                // we insert 2 edge updates per edge
-  ofstream out{"runtime_data.txt"}; // open the outfile
-
-  printf("Insertions\n");
-  printf("Progress:                    | 0%%\r"); fflush(stdout);
-  std::chrono::steady_clock::time_point start = start_time;
-  uint64_t prev_updates = 0;
-
-  while(true) {
-    sleep(5);
-    uint64_t updates = g->num_updates;
-    std::chrono::steady_clock::time_point now = std::chrono::steady_clock::now();
-    std::chrono::duration<double> diff = now - start;
-    start = now; // reset start time to right after query
-
-    // calculate the insertion rate and write to file
-    uint64_t upd_delta = updates - prev_updates;
-    // divide insertions per second by 2 because each edge is split into two updates
-    // we care about edges per second not about stream updates
-    float ins_per_sec = (((float)(upd_delta)) / diff.count()) / 2;
-
-    prev_updates += upd_delta;
-    out << (prev_updates / 2) << "\t" << ins_per_sec << "\n";
-    
-    if (updates >= total)
-      break;
-
-    // display the progress
-    int percent = updates / (total * .05);
-    printf("Progress:%s%s", std::string(percent, '=').c_str(), std::string(20 - percent, ' ').c_str());
-    printf("| %i%% -- %.2f per second\r", percent * 5, ins_per_sec); fflush(stdout);
-  }
-  printf("Progress:====================| Done\n");
-  std::chrono::duration<double> runtime = g->end_time - start_time;
-
-  // calculate the insertion rate and write to file
-  float ins_per_sec = (((float)(total)) / runtime.count()) / 2;
-  out << "DONE\n";
-  out << (total / 2) << "\t" << ins_per_sec << "\n";
-  out.close();
-  return;
-}
-
-
 TEST(Benchmark, BCHMKGraph) {
   const std::string fname = __FILE__;
   size_t pos = fname.find_last_of("\\/");
@@ -79,7 +26,6 @@ TEST(Benchmark, BCHMKGraph) {
   Graph g{num_nodes};
 
   std::chrono::steady_clock::time_point start = std::chrono::steady_clock::now();
-  std::thread querier(query_insertions, total, &g, start);
 
   while (m--) {
     in >> u >> a >> b;
@@ -89,7 +35,15 @@ TEST(Benchmark, BCHMKGraph) {
       g.update({{a,b}, DELETE});
   }
   uint64_t num_CC = g.connected_components().size();
-
-  querier.join();
   printf("Number of connected components is %lu\n", num_CC);
+
+  ofstream out{"runtime_data.txt"}; // open the outfile
+  std::chrono::duration<double> runtime = g.end_time - start;
+
+  // calculate the insertion rate and write to file
+  // insertion rate measured in stream updates 
+  // (not in the two sketch updates we process per stream update)
+  float ins_per_sec = (((float)(total)) / runtime.count()) / 2;
+  out << (total / 2) << "\t" << ins_per_sec << "\n";
+  out.close();
 }
--- test/experiment/run_experiments.sh
+++ test/experiment/run_experiments.sh
@@ -1,40 +1,30 @@
 #! /bin/bash
 
+# run the experiments as follows
+# test/experiment/run_experiments [executable] [input_files]
+# executable = ./experiment_memory or ./experiment_external
+# input_files = the graph streams to test upon
+
 # The specific test we want to run
 executable=$1
 
 # for every input file specified on the command line
 for input in "${@:2}"
 do
-	echo ""
 	echo "input file: $input"
-	
-	mkdir test_`basename $input`_data
-	cp $input test/res/current_test.stream
-	total=1
-	for ((j=0; j < 5; j++))
-	do
-		for ((size=1; size <= total; size*=2))
-		do
-			groups=$((total/size))
 
-			start=`date +%s`
-			
-			echo "path_prefix=./BUFFTREEDATA/" > streaming.conf
-			echo "num_groups=$((groups))"      >> streaming.conf
-			echo "group_size=$((size))"        >> streaming.conf
-			
-			"$executable"
-			
-			mv runtime_data.txt test_`basename $input`_data/g$((groups))_s$((size))_data.txt
-			
-			end=`date +%s`
-		done
-		total=$((total*2))
-	done
+	# TODO: put the code for running the memory profiler here
+	# & top ... | awk ... 
+	# TODO: pipe the above output to a file
+	
+	mkdir test_`basename $input`
+	ln -sf $input test/res/current_test.stream
+	
+	"$executable" # either experiment_memory or experiment_external
+	
+	mv runtime_data.txt test_`basename $input`/
+	# TODO: copy the awk output over as well.
 done
 
 # delete buffer tree data if it exists
 rm BUFFTREEDATA/*
-
-
