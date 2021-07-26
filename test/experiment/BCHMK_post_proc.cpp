e8cc10130d41018313ce7a58c6f9082125474403
Victor Zhang <vzzhang@amazon.com>
2021-07-25 19:49:51 -0700

    kron 13

--- test/experiment/BCHMK_post_proc.cpp
+++ test/experiment/BCHMK_post_proc.cpp
@@ -7,7 +7,8 @@ TEST(Benchmark, BCHMKpostProcSingleOnPaperclipGraph) {
   const std::string fname = __FILE__;
   size_t pos = fname.find_last_of("\\/");
   const std::string curr_dir = (std::string::npos == pos) ? "" : fname.substr(0, pos);
-  std::ifstream in{curr_dir + "/../res/paperclip.stream"};
+//  std::ifstream in{curr_dir + "/../res/paperclip.stream"};
+  std::ifstream in{"/home/experiment_inputs/streams/kron_13_unique_half_stream.txt"};
   Node num_nodes;
   in >> num_nodes;
   long m;
@@ -18,5 +19,5 @@ TEST(Benchmark, BCHMKpostProcSingleOnPaperclipGraph) {
     in >> a >> b;
     g.update({{a, b}, INSERT});
   }
-  std::cout << "Number of CCs: " << g.connected_components().size() <<std::endl;
+  std::cout << "Number of CCs: " << g.connected_components().size() <std::endl;
 }
