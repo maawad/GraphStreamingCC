19aebe2db7fd803a21fbd7119a975edfd0791bb3
Victor Zhang <vzzhang@amazon.com>
2021-07-25 18:46:23 -0700

    expr: paperclip on master

---
+++ test/experiment/BCHMK_post_proc.cpp
@@ -0,0 +1,22 @@
+#include <gtest/gtest.h>
+#include <fstream>
+#include "../../include/supernode.h"
+#include "../../include/graph.h"
+
+TEST(Benchmark, BCHMKpostProcSingleOnPaperclipGraph) {
+  const std::string fname = __FILE__;
+  size_t pos = fname.find_last_of("\\/");
+  const std::string curr_dir = (std::string::npos == pos) ? "" : fname.substr(0, pos);
+  std::ifstream in{curr_dir + "/../res/paperclip.stream"};
+  Node num_nodes;
+  in >> num_nodes;
+  long m;
+  in >> m;
+  Node a, b;
+  Graph g{num_nodes};
+  while (m--) {
+    in >> a >> b;
+    g.update({{a, b}, INSERT});
+  }
+  std::cout << g.connected_components().size() << std::endl;
+}
